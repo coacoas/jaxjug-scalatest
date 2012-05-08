df53dee796ec99d4b452466d440fe19bac8c7665
Bill Carlson <bill@coacoas.net>
2012-05-08 02:08:49 -0400

    Setting the Scala sample app back to the way it's supposed to be, not the nasty 'make-it-look-more-like-java' way

--- src/main/scala/com/jaxjug/ScalaSample.scala
+++ src/main/scala/com/jaxjug/ScalaSample.scala
@@ -1,13 +1,9 @@
 package com.jaxjug;
 
-object ScalaSample {
-	def main(args: Array[String]) {
-		def sayHello(name: String): String = {
-			"Hello, %s!".format(name);
-		}
+object ScalaSample extends App {
+	def sayHello(name: String) = "Hello, %s!".format(name);
 
-		val audience = "JaxJUG";
+	val audience = "JaxJUG";
 
-		println(sayHello(audience));
-	}
+	println(sayHello(audience));
 }
